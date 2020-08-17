f133dab61e8ff04793a0e00290edffb06a948722
Inviseble057 <69232202+Inviseble057@users.noreply.github.com>
2020-08-17 15:10:16 +0000

    Create TrojanFake.py

---
+++ TrojanFake.py
@@ -0,0 +1,59 @@
+#!/bin/user/python
+import os
+import sys
+import time
+
+load = '+'
+count = 0
+
+for x in range(21):
+	time.sleep(0.7)
+	print(f'\nLOADING {x}% [{load}]', end='', flush=True)
+	count += 1
+	if count == 3:
+	    count = 0
+	    load += '+'
+os.system("clear")
+ggwp =input("masukkan passwordnya lurr :")
+if ggwp == "TheGrayhat":
+	print ("[√] login succses")
+else:
+	print ("[X] login vailed")
+
+time.sleep(0.7)
+os.system("clear")
+os.system("figlet FAKE")
+time.sleep(2)
+print ("[=]Menu pilihan[=]")
+print ("(1).Melunjur Hayyuukkk")
+print ("(2).tentang script ini")
+print ("(3).exit/keluar")
+ggwp =input("Mr_Inviseble>>")
+if ggwp == '1':
+	os.system("clear")
+	name =input("masukkan nomer tujuan>> ")
+	profile =input("masukkan jumlah>> ")
+	while True:
+		print ("sedang mengirim trojan ke",name,"sebanyak",profile)
+	time.sleep(0.5)
+elif ggwp == '2':
+	os.system("clear")
+	print ("Script ini hanyalah script TROJANS fake/palsu karna jika nanti mungkin saya menyiptakan script trojans beneran akan disalah gunakan!")
+	print ("jika ingin selesai tekan CTRL,dan tekan C pada keyboard anda,paham!")
+	print ("o ya,saya lupa memberitahu script ini juga untuk pank teman anda")
+	print ("wkwkwkwkwkwkkwkw")
+	print (" I am Mr_Inviseble ")
+	print (" The Grayhat ")
+	print (" saya undur diri,sekian dan terimakasih ")
+	print ("SALAM_CYBER,SALAM_HACKER,SALAM_MUSLIM")
+	time.sleep(0.5)
+elif ggwp == '3':
+	print ("tinggal ketik CD aja susah!")
+	print ("saya memberitahu bukan nge-gass kambing")
+	time.sleep(0.5)
+else:
+	print ("Baku hantam yuuk")
+	time.sleep(0.2)
+	print ("IG saya : _hr_art_")
+	print ("jangan lupa like,share,comment,subscribe,and follow")
+
